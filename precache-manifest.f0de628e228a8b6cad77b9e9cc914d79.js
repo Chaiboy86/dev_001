9f215cee60b48e11f5882f51dd19e4396b37d3d2
Chaiboy86 <kazmi.yamazato@gmail.com>
2020-03-21 16:22:45 +0900

    Updates

---
+++ precache-manifest.f0de628e228a8b6cad77b9e9cc914d79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877671d50d7f3753bc21aee4b8f4cff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f4639311913ae59ae78",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f22878bb461762d644ad",
+    "url": "/static/js/2.cee9e930.chunk.js"
+  },
+  {
+    "revision": "d4fb81d481b0c6ecaf1ed1cc9679f693",
+    "url": "/static/js/2.cee9e930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f4639311913ae59ae78",
+    "url": "/static/js/main.bba98e92.chunk.js"
+  },
+  {
+    "revision": "aa3074ccc8efa6a1e6ee",
+    "url": "/static/js/runtime-main.6f8930a6.js"
+  }
+]);
